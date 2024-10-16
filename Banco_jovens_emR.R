2ff1386cc82191f203b5591625bfb473b2085660
Mariana Kaori <87149445+marikso@users.noreply.github.com>
2024-10-16 09:50:41 -0300

    Atualizar o Banco_jovens_emR.R

--- Banco_jovens_emR.R
+++ Banco_jovens_emR.R
@@ -15,7 +15,7 @@ library(car)
 library(rstudioapi)
 
 # read_sav() lê arquivo SSPS
-banco <- read_sav("banco.sav")
+banco <- read_sav("banco_jovens.sav")
 
 # Função apenas para exibir o menu de interação
 menu <- function() {
